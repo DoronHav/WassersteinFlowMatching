fix: add fill_triangular and fill_triangular_inverse imports; update BuresWassersteinFlowMatching and InputMeanCovarianceNN to use triangular covariance representation; fix num_sinkhorn_iter variable name in WassersteinFlowMatching; update test parameterization for noise types

src/wassersteinflowmatching/bures_wasserstein/BuresWassersteinFlowMatching.py
@@ -13,7 +13,7 @@ from flax.training import train_state # type: ignore
 import wassersteinflowmatching.bures_wasserstein.utils_OT as utils_OT # type: ignore
 import wassersteinflowmatching.bures_wasserstein.utils_Noise as utils_Noise # type: ignore
 import wassersteinflowmatching.bures_wasserstein.utils_Pointclouds as utils_Pointclouds # type: ignore
-from wassersteinflowmatching.bures_wasserstein._utils_Neural import BuresWassersteinNN # type: ignore
+from wassersteinflowmatching.bures_wasserstein._utils_Neural import BuresWassersteinNN, fill_triangular, fill_triangular_inverse # type: ignore
 from wassersteinflowmatching.bures_wasserstein.DefaultConfig import DefaultConfig # type: ignore
 
 
@@ -215,11 +215,17 @@ class BuresWassersteinFlowMatching:
         interpolates_means, interpolates_covariances  = self.mccann_interpolation_jit([means_noise, covariances_noise], [A_flow, b_flow], 1 - interpolates_time)
         interpolates_means_dot, interpolates_covariances_dot = self.mccann_derivative_jit([means_noise, covariances_noise], [A_flow, b_flow], 1 - interpolates_time)
 
+        interpolates_covariances_tril = self.vmapped_fill_triangular_inverse(interpolates_covariances)
+        interpolates_covariances_dot_tril = self.vmapped_fill_triangular_inverse(interpolates_covariances_dot)
+
         subkey, key = random.split(key)
-        def loss_fn(params):       
+        def loss_fn(params):   
+
+            
+
             predicted_mean_dot, predicted_cov_dot = state.apply_fn({"params": params},  
                                             means = interpolates_means, 
-                                            covariances = interpolates_covariances,
+                                            covariances = interpolates_covariances_tril,
                                             t = interpolates_time, 
                                             labels = labels_batch,
                                             deterministic = False, 
@@ -227,7 +233,7 @@ class BuresWassersteinFlowMatching:
             
             
             mean_loss, cov_loss = self.loss_func([-predicted_mean_dot, -predicted_cov_dot], 
-                                                [interpolates_means_dot, interpolates_covariances_dot],
+                                                [interpolates_means_dot, interpolates_covariances_dot_tril],
                                                 [interpolates_means, interpolates_covariances])
                             
             loss = jnp.mean(mean_loss) + jnp.mean(cov_loss)
@@ -265,6 +271,9 @@ class BuresWassersteinFlowMatching:
         subkey, key = random.split(key)
 
         self.FlowMatchingModel = BuresWassersteinNN(config = self.config)
+        self.vmapped_fill_triangular = jax.jit(jax.vmap(fill_triangular, (0,), 0))
+        self.vmapped_fill_triangular_inverse = jax.jit(jax.vmap(fill_triangular_inverse, (0,), 0))
+
         self.state = self.create_train_state(model = self.FlowMatchingModel,
                                              learning_rate=learning_rate, 
                                              decay_steps = decay_steps, 
@@ -323,12 +332,13 @@ class BuresWassersteinFlowMatching:
             means = means[None, :]
             covariances = covariances[None, :]  
 
-        flow_mean, flow_cov = self.FlowMatchingModel.apply({"params": self.params},
+        flow_mean, flow_cov_tril = self.FlowMatchingModel.apply({"params": self.params},
                     means = means, 
                     covariances = covariances,
                     t = t * jnp.ones(covariances.shape[0]), 
                     labels = labels,
                     deterministic = True)
+        flow_cov = self.vmapped_fill_triangular(flow_cov)
         flow_mean,flow_cov = jnp.squeeze(flow_mean), jnp.squeeze(flow_cov)
 
         return([flow_mean, flow_cov])

src/wassersteinflowmatching/bures_wasserstein/_utils_Neural.py
@@ -131,15 +131,13 @@ class InputMeanCovarianceNN(nn.Module):
     config: DefaultConfig
 
     @nn.compact
-    def __call__(self, means, covariances, t,  labels = None, deterministic = True):
+    def __call__(self, means, cov_tril, t,  labels = None, deterministic = True):
         config = self.config
 
         embedding_dim = config.embedding_dim
         num_layers = config.num_layers
 
         freqs = jnp.arange(embedding_dim//2) 
-
-        cov_tril = vmapped_fill_triangular_inverse(covariances)
 
         means_emb = nn.Dense(features = embedding_dim)(means)
         covariances_emb = nn.Dense(features = embedding_dim)(cov_tril)
@@ -185,7 +183,7 @@ class BuresWassersteinNN(nn.Module):
                         bias_init=nn.initializers.zeros)(mean_dot_emb)
 
 
-            tril_vec = nn.Dense((space_dim * (space_dim + 1)) // 2,
+            covariance_dot_tril = nn.Dense((space_dim * (space_dim + 1)) // 2,
                                 kernel_init=nn.initializers.variance_scaling(1e-3, mode='fan_in', distribution='truncated_normal'), 
                                 bias_init=nn.initializers.zeros)(sigma_dot_emb)
 
@@ -196,11 +194,8 @@ class BuresWassersteinNN(nn.Module):
 
 
             mean_dot = nn.Dense(space_dim)(dot_emb)
-            tril_vec = nn.Dense((space_dim * (space_dim + 1)) // 2)(dot_emb)
+            covariance_dot_tril = nn.Dense((space_dim * (space_dim + 1)) // 2)(dot_emb)
 
-        lower_triangular = vmapped_fill_triangular(tril_vec)
-        covariance_dot = lower_triangular + jnp.triu(lower_triangular.transpose([0,2,1]), k=1)
-
-        return mean_dot, covariance_dot
+        return mean_dot, covariance_dot_tril
 
 

src/wassersteinflowmatching/wasserstein/WassersteinFlowMatching.py
@@ -106,7 +106,7 @@ class WassersteinFlowMatching:
         self.monge_map = self.config.monge_map
         self.num_sinkhorn_iters = self.config.num_sinkhorn_iters
 
-        if(self.num_sinkhorn_iter == -1):
+        if(self.num_sinkhorn_iters == -1):
             print("Automatically setting num_sinkhorn_iter by testing OT function")
 
             subkey_noise, key = random.split(key)
@@ -115,14 +115,14 @@ class WassersteinFlowMatching:
                                        key = subkey_noise)
             noise_weights = jnp.ones([noise_point_clouds.shape[0], noise_point_clouds.shape[1]]) / noise_point_clouds.shape[1]
 
-            self.num_sinkhorn_iter = utils_OT.auto_find_num_iter(point_clouds = self.point_clouds, 
+            self.num_sinkhorn_iters = utils_OT.auto_find_num_iter(point_clouds = self.point_clouds, 
                                                                  weights = self.weights,
                                                                  noise_point_clouds = noise_point_clouds,
                                                                 noise_weights = noise_weights,
                                                                  eps = self.config.wasserstein_eps, lse_mode = self.config.wasserstein_lse)
-            print("Setting num_sinkhorn_iter to", self.num_sinkhorn_iter)
+            print("Setting num_sinkhorn_iter to", self.num_sinkhorn_iters)
         else:
-            print("Using num_sinkhorn_iter =", self.num_sinkhorn_iter) 
+            print("Using num_sinkhorn_iter =", self.num_sinkhorn_iters) 
 
         if(self.monge_map == 'entropic'):
             print(f"Using entropic map with {self.num_sinkhorn_iters} iterations and {self.config.wasserstein_eps} epsilon")